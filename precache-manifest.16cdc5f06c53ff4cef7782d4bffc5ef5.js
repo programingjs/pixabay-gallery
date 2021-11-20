6ba814f2a10b7573860b38feeac1183b57ed2aae
software369 <83788939+software369@users.noreply.github.com>
2021-11-19 22:35:10 -0500

    Updates

---
+++ precache-manifest.16cdc5f06c53ff4cef7782d4bffc5ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af10f084c530d52d4a0e949964c02392",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63e7668784fd91d81feb",
+    "url": "/static/css/main.096afcfd.chunk.css"
+  },
+  {
+    "revision": "5d50c1a52638fbda4191",
+    "url": "/static/js/2.3f381743.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f381743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e7668784fd91d81feb",
+    "url": "/static/js/main.46a6eefe.chunk.js"
+  },
+  {
+    "revision": "a9d5425ff6bd1228cece",
+    "url": "/static/js/runtime-main.d120cc81.js"
+  }
+]);
